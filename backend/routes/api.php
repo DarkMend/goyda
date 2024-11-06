477d300e9426cee72f1ee81ce0e91381b772495e
DarkMend <gindullin20@mail.ru>
2024-11-06 12:39:49 +0300

    06/11 back-1

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/catalog', [ProductController::class, 'index']);

bed4a5057dcb96ee49fbe599e80843317848c7e8
DarkMend <gindullin20@mail.ru>
2024-11-10 19:25:47 +0300

    modal is work

--- frontend/src/components/ModalLayout/ModalLayout.tsx
+++ frontend/src/components/ModalLayout/ModalLayout.tsx
@@ -4,23 +4,34 @@ import { IModalLayout } from './ModalLayout.props';
 import { X } from 'lucide-react';
 import cn from 'classnames';
 import { store } from '../../store/store';
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+import { useDispatch } from 'react-redux';
+import { modalActions } from '../../store/modal.slice';
 
 export default function ModalLayout({ children }: IModalLayout) {
 
-    let isActive = store.getState().modal.isActive;
+    const [, forceUpdate] = useReducer((x) => x + 1, 0)
+    const dispatch = useDispatch();
 
-    useEffect(() => {store.subscribe(() => {
-        isActive = store.getState().modal.isActive
-    })}, [store.subscribe]);
+    useEffect(() => {
+        const unsubscribe = store.subscribe(() => {
+            forceUpdate();
+        })
+
+        return unsubscribe;
+    }, [store.subscribe]);
+
+    const closeModal = () => {
+        dispatch(modalActions.setIsActive(!store.getState().modal.isActive))
+    }
 
     return createPortal(
         <div className={cn(styles['modal'], {
-            [styles['active']]: isActive
+            [styles['active']]: store.getState().modal.isActive
         })}>
             <div className={styles['content']}>
                 <div className={styles['close-wrapper']}>
-                    <button className={styles['close']}>
+                    <button className={styles['close']} onClick={closeModal}>
                         <X />
                     </button>
                 </div>

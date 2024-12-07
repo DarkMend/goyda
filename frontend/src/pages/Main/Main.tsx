afdc48c7a1f0b363db5b0e0c4d0da92620a246df
DarkMend <gindullin20@mail.ru>
2024-12-08 00:11:52 +0300

    auth fix

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -3,17 +3,19 @@ import Title from '../../components/Title/Title'
 import styles from './Main.module.css'
 import { axiosWithAuth } from '../../api/interceptors'
 
+
+
 export default function Main() {
 
     const {data} = useQuery({
         queryKey: ['token'],
-        queryFn: () => axiosWithAuth.get('http://127.0.0.1:8000/api/token')
+        queryFn: () => axiosWithAuth.get(`/token`)
     })
 
     return (
         <div className={styles['main']}>
             <Title>Добпро епта</Title>
-            <div>{data?.data}</div>
+            <div>{data?.data.id}</div>
         </div>
     )
 }

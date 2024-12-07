7f78d3a1cbac892c013a563532cb504dca5392bc
DarkMend <gindullin20@mail.ru>
2024-12-07 15:20:36 +0300

    auth

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import Title from '../../components/Title/Title'
 import styles from './Main.module.css'
+import { axiosWithAuth } from '../../api/interceptors'
 
 export default function Main() {
+
+    const {data} = useQuery({
+        queryKey: ['token'],
+        queryFn: () => axiosWithAuth.get('http://127.0.0.1:8000/api/token')
+    })
+
     return (
         <div className={styles['main']}>
             <Title>Добпро епта</Title>
+            <div>{data?.data}</div>
         </div>
     )
 }

9e0da447a9f506bf2ec4631b881046749c3da7ed
DarkMend <gindullin20@mail.ru>
2024-12-21 16:09:09 +0300

    slider

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -2,18 +2,18 @@ import { useSelector } from 'react-redux'
 import Title from '../../components/Title/Title'
 import styles from './Main.module.css'
 import { RootState } from '../../store/store'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
 
 
 
 export default function Main() {
 
-    const state = useSelector<RootState>(state => state.user)
-
     return (
         <div className={styles['main']}>
             <Title>Добпро епта</Title>
             <div>
-
+                
             </div>
         </div>
     )
